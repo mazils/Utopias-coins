ffc5947e6be08dbb589fa6254566739f31542f28
Mazils <arturasmaziliauskas2@gmail.com>
2020-11-22 22:04:23 +0100

    dynamic programming implemented

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,25 +3,26 @@ import java.util.HashMap;
 public class Main
 {
     private static HashMap<Integer, Integer> hashMap;
-
+    private static HashMap<Integer, Integer> pastValuesHashMap;
 
     public static void main(String[] args)
     {
         hashMap = new HashMap<>();
-        Main utopiaCoins = new Main();
+        pastValuesHashMap = new HashMap<>();
+
         hashMap.put(1, 1);
         hashMap.put(7, 1);
         hashMap.put(10, 1);
         hashMap.put(22, 1);
 
-        for (int i = -6; i < 70; i++)
+        for (int i = 2; i < 1000000000; i++)
         {
-            System.out.println("Result for number :" + i + "  " + utopiaCoins.findChange(i));
+            System.out.println("Result for number :" + i + "  " + findChange(i));
         }
 
     }
 
-    public int findChange(int amount)
+    public static int findChange(int amount)
     {
         int tempMinChange = 0;
         int minChange = 0;
@@ -34,8 +35,8 @@ public class Main
 
         if (amount < 0)
             return 0;
-        if (hashMap.containsKey(amount))
-            return hashMap.get(amount);
+        if (pastValuesHashMap.containsKey(amount))
+            return pastValuesHashMap.get(amount);
 
         for (Integer key : hashMap.keySet())
         {
@@ -51,7 +52,7 @@ public class Main
             }
 
         }
-
+        pastValuesHashMap.put(amount,minChange);
         return minChange;
     }
 }
