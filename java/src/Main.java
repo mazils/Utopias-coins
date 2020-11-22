bc3221874ffa946c9f3fa54ba0710918b33d0fa7
Mazils <arturasmaziliauskas2@gmail.com>
2020-11-22 21:47:38 +0100

    finished

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,7 +1,57 @@
+import java.util.HashMap;
+
 public class Main
 {
+    private static HashMap<Integer, Integer> hashMap;
+
+
     public static void main(String[] args)
     {
-        System.out.println("bybis");
+        hashMap = new HashMap<>();
+        Main utopiaCoins = new Main();
+        hashMap.put(1, 1);
+        hashMap.put(7, 1);
+        hashMap.put(10, 1);
+        hashMap.put(22, 1);
+
+        for (int i = -6; i < 70; i++)
+        {
+            System.out.println("Result for number :" + i + "  " + utopiaCoins.findChange(i));
+        }
+
+    }
+
+    public int findChange(int amount)
+    {
+        int tempMinChange = 0;
+        int minChange = 0;
+
+        if (amount == 0)
+        {
+
+            return 0;
+        }
+
+        if (amount < 0)
+            return 0;
+        if (hashMap.containsKey(amount))
+            return hashMap.get(amount);
+
+        for (Integer key : hashMap.keySet())
+        {
+            int var = amount - key;
+            if (var > 0)
+            {
+                tempMinChange = findChange(var) + 1;
+                if (tempMinChange < minChange || minChange == 0)
+                {
+                    minChange = tempMinChange;
+                }
+
+            }
+
+        }
+
+        return minChange;
     }
 }
